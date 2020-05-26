554d5737a9ea9a4561c03f6f9a039ccf4b4487a5
BenMatela <matelaben@gmail.com>
2020-05-26 13:10:29 +0200

    downloading notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { PdfViewerModule } from 'ng2-pdf-viewer'; 
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer'; 
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
