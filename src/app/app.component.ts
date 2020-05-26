ac79eb29cff4ce746b8e505ad7ae09ef5fa0bfd9
BenMatela <matelaben@gmail.com>
2020-05-26 15:13:54 +0200

    created templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
 
@@ -9,16 +9,29 @@ import html2canvas from 'html2canvas';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isReaderMode: boolean = false;
   page: number = 1;
   totalPages: number;
   isLoaded: boolean = false;
   notes: string = '';
   src: string = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event) {
+    // 200 is the height from bottom from where you want to trigger the infintie scroll, can we zero to detect bottom of window
+    if ((document.body.clientHeight + window.scrollY + 200) >= document.body.scrollHeight) {
+        console.log('tiggred');
+    }
+  }
+
   // saveIcon = faSave;
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.isReaderMode);
   }
 
   afterLoadComplete(pdfData: any) {
@@ -42,7 +55,7 @@ export class AppComponent {
       const pageHeight = pdf.internal.pageSize.height || pdf.internal.pageSize.getHeight();
       const pageWidth = pdf.internal.pageSize.width || pdf.internal.pageSize.getWidth();
       const position = 0;
-      const footerText = "Explo Dash by Grind Square @" + new Date().getFullYear();
+      const footerText = "Doc Reader - " + new Date().getFullYear();
       
       pdf.setTextColor(100);
       pdf.setFontSize(10);
